0966b39f0f9852370f7c7be0089ba928e4f0be22
scott.liao <shazi7804@gmail.com>
2017-09-19 17:46:53 +0800

    fix centos5/6

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,14 +1,17 @@
 class corp104_ssm_agent::install inherits corp104_ssm_agent {
+
+  $ssm_agent_download_url = "https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/${corp104_ssm_agent::osver}/amazon-ssm-agent.${corp104_ssm_agent::package_type}"
+
   if $corp104_ssm_agent::proxy_install_manage {
     exec { 'download_ssm-agent':
-      command => "curl -x ${corp104_ssm_agent::http_proxy} --connect-timeout ${corp104_ssm_agent::proxy_install_manage_timeout} -o ${corp104_ssm_agent::ssm_tmp_file} -O ${corp104_ssm_agent::ssm_agent_download_url}",
+      command => "curl -x ${corp104_ssm_agent::http_proxy} --connect-timeout ${corp104_ssm_agent::proxy_install_manage_timeout} -o ${corp104_ssm_agent::ssm_tmp_file} -O ${ssm_agent_download_url}",
       path    => '/bin:/usr/bin:/usr/local/bin:/usr/sbin',
       creates => $corp104_ssm_agent::ssm_tmp_file
     }
   }
   else {
     exec { 'download_ssm-agent':
-      command => "curl -o ${corp104_ssm_agent::ssm_tmp_file} --connect-timeout ${corp104_ssm_agent::proxy_install_manage_timeout} -O ${corp104_ssm_agent::ssm_agent_download_url}",
+      command => "curl -o ${corp104_ssm_agent::ssm_tmp_file} --connect-timeout ${corp104_ssm_agent::proxy_install_manage_timeout} -O ${ssm_agent_download_url}",
       path    => '/bin:/usr/bin:/usr/local/bin:/usr/sbin',
       creates => $corp104_ssm_agent::ssm_tmp_file
     }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 
 describe 'corp104_ssm_agent', :type => 'class' do
   context 'with defaults for all parameters' do
-    let(:facts) do { 
-      :osfamily => 'Debian',
-    }
+    let(:facts) do
+      {
+        :os => { :family => 'Debian', :name => 'Ubuntu', :release => { :major => '16.04', :full => '16.04' }},
+        :lsbdistrelease  => '16.04',
+        :lsbdistid       => 'Ubuntu',
+        :osfamily        => 'Debian',
+        :lsbdistcodename => 'xenial',
+      }
     end
     it do
       should contain_class('corp104_ssm_agent')
